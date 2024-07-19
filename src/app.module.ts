3fef159564eb8c7d57eb287c65d342a9fb488edb
skyispy <skyispy1003@gmail.com>
2024-07-19 15:18:41 +0900

    준비

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ItemModule],
   controllers: [AppController],
   providers: [AppService],
 })
