8329509fe471f683249e265cd64176638956b6ad
pvekxx <https://github.com/pvekxx>
2024-07-23 11:49:53 +0900

    merge

--- src/auth/strategy/kakao.strategy.ts
+++ src/auth/strategy/kakao.strategy.ts
@@ -1,8 +1,16 @@
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-kakao"
 
 @Injectable()
 export class KakaoStrategy extends PassportStrategy(Strategy, "kakao") {
+    constructor(private configService: ConfigService) {
+        super({
+            clientID: configService.get<string>("KAKAO_CLIENT_ID"),
+            callbackURL: configService.get<string>("KAKAO_CALLBACK_URL"),
+        })
+    }
 
+    validate
 }
