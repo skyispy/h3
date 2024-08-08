c0755c223bdcbf3b9a6a51bdd7deee4f2ab18957
skyispy <skyispy1003@gmail.com>
2024-08-08 13:23:06 +0900

    탈퇴까지

--- src/public/ts/signup.d.ts
+++
@@ -1,5 +0,0 @@
-declare const signupBtn: HTMLButtonElement;
-declare const checkAll: HTMLInputElement;
-declare const checks: NodeListOf<Element>;
-declare const duplicateCheckBtn: HTMLButtonElement;
-declare const duplicateCheckBtn2: HTMLButtonElement;
